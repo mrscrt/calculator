818295d081b8609114fc6474b83063524d8cae67
mrscrt <139079575+mrscrt@users.noreply.github.com>
2023-07-21 16:24:01 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Ошибка: деление на ноль недопустимо"
+
+def main():
+    print("Простой калькулятор")
+    print("===================")
+
+    while True:
+        print("Выберите операцию:")
+        print("1. Сложение")
+        print("2. Вычитание")
+        print("3. Умножение")
+        print("4. Деление")
+        print("0. Выход")
+
+        choice = input("Введите номер операции: ")
+
+        if choice == "0":
+            print("До свидания!")
+            break
+
+        if choice in ("1", "2", "3", "4"):
+            num1 = float(input("Введите первое число: "))
+            num2 = float(input("Введите второе число: "))
+
+            if choice == "1":
+                print("Результат:", add(num1, num2))
+            elif choice == "2":
+                print("Результат:", subtract(num1, num2))
+            elif choice == "3":
+                print("Результат:", multiply(num1, num2))
+            elif choice == "4":
+                print("Результат:", divide(num1, num2))
+        else:
+            print("Ошибка: неверный выбор операции. Повторите попытку.")
+
+if __name__ == "__main__":
+    main()
